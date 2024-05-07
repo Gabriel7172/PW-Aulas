da23c347031797b04853d4c45de8982747eff9c1
Gabriel Schultes <134557881+Gabriel7172@users.noreply.github.com>
2024-05-07 13:14:34 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,19 @@
+//exercicio 1
+for(var i = 1; i <=100; i++){
+    if(i%2==0){
+    console.log('par',i)
+    }else{
+        console.log('impar',i)
+    }
+}
+
+//exercicio 2
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+
+for(var e=0;e<=10;e++){
+    if(e%2==0){
+        console.log('imparvet',e,arr[e]);
+    }
+    
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+// Data: 09/04/2024
+// Autor: Gabriel Schultes - RM 2301611
+
+//----- Estrutura Repetição Simples for -----
+
+//for(var i = 0; i <= 10; i++){console.log(i);}
+
+
+//----- vetor ou array -----
+
+var arr = [1,2,3,4,5];
+
+for(var i = 0; i<=4;i++){console.log(i, '=', arr[i]);}
+   
+
